7783a4b84fe0d328093ac3eb9a4334fd3cee9b26
Matt Dwyer <mattsd1410@gmail.com>
2024-09-22 21:40:13 +1000

    implement the tailwind scaffold based on the current `tempest/app` repo

--- src/Templates/Tailwind/TailwindTemplate.php
+++ src/Templates/Tailwind/TailwindTemplate.php
@@ -3,12 +3,16 @@
 namespace Tempest\Scaffold\Templates\Tailwind;
 
 use Tempest\Console\Console;
+use Tempest\Core\Kernel;
 use Tempest\Scaffold\Template;
 
 final readonly class TailwindTemplate
 {
+	const TAILWIND_NPM_VERSION = '^3.4.1';
+
     public function __construct(
         private readonly Console $console,
+		private readonly Kernel $kernel,
     ) {}
 
 	#[Template(
@@ -16,6 +20,85 @@ final readonly class TailwindTemplate
 	)]
 	public function __invoke(): void
 	{
-		$this->console->writeln('hello tailwind');
+		$this->setupNpm();
+
+		$this->configureTailwind();
+
+		$this->writeDummyCss();
+
+		// TODO: wrap this one in an "is post-create-project-cmd hook" check
+		$this->rewriteHomeView();
+
+		$this->console->writeln('Done!')
+			->info('Run `npm install` to complete the installation, then `npm run dev` to compile your Tailwind styles');
+	}
+
+	private function setupNpm(): void
+	{
+		$this->console->writeln('Adding tailwindcss to your package.json');
+
+		$packageJsonPath = "{$this->kernel->root}/package.json";
+
+		$packageJson = @file_get_contents($packageJsonPath);
+
+		$data = @json_decode(
+			json: $packageJson,
+			associative: true,
+		);
+
+		if (! $data) {
+			$data = [
+				'scripts' => [ ],
+				'devDependencies' => [ ],
+			];
+		}
+
+		$data['scripts']['dev'] = 'npx tailwindcss -i ./app/main.css -o ./public/main.css --watch';
+		$data['scripts']['build'] = 'npx tailwindcss -i ./app/main.css -o ./public/main.css';
+
+		$data['devDependencies']['tailwindcss'] = self::TAILWIND_NPM_VERSION;
+
+		$packageJson = json_encode(
+			value: $data,
+			flags: JSON_PRETTY_PRINT,
+		);
+
+		file_put_contents($packageJsonPath, $packageJson);
+	}
+
+	private function configureTailwind(): void
+	{
+		$this->console->writeln('Adding a tailwind.config.js file');
+
+		$src = __DIR__;
+		$src = "{$src}/tailwind.config.js";
+
+		$dst = "{$this->kernel->root}/tailwind.config.js";
+
+		copy($src, $dst);
+	}
+
+	private function writeDummyCss(): void
+	{
+		$this->console->writeln('Adding a dummy css file');
+
+		$src = __DIR__;
+		$src = "{$src}/main.css";
+
+		$dst = "{$this->kernel->root}/app/main.css";
+
+		copy($src, $dst);
+	}
+
+	private function rewriteHomeView(): void
+	{
+		$this->console->writeln('Replacing your app/home.view.php file');
+
+		$src = __DIR__;
+		$src = "{$src}/home.view.php";
+
+		$dst = "{$this->kernel->root}/app/home.view.php";
+
+		copy($src, $dst);
 	}
 }
